068cf0a86fa97d5806e494af5425cc2c00249c30
조연희 <yhcho.proj@gmail.com>
2025-12-04 12:18:48 +0900

    게시물 이미지 추가

--- myhomepage/src/pages/BoardWrite.jsx
+++ myhomepage/src/pages/BoardWrite.jsx
@@ -33,7 +33,7 @@ import {handleChangeImage, handleInputChange} from "../service/commonService";
  *  }
  */
 /*
-TODO 게시물 작성하기 에서 게시물 관련 이미지 추가 넣기
+게시물 작성하기 에서 게시물 관련 이미지 추가 넣기
 1. 게시물 작성할 때, 게시물 이미지 추가하기와 같은 라벨 추가
 2. 라벨을 선택했을 때, 이미지만 선택 가능하도록 input 실행
 3. input = display:none;
@@ -41,22 +41,31 @@ TODO 게시물 작성하기 에서 게시물 관련 이미지 추가 넣기
 
 등록하기를 했을 경우에만 추가하기 가능
 */
+
 const BoardWrite = () => {
-    const {user, isAuthenticated, logoutFn} = useAuth();
     // form 데이터 내부 초기값
     // 작성자 -> 추후 변경 불가하게 로그인한 아이디로 박제 예정
     // react-router-dom 에 존재하는 path 주소 변경 기능 사용
     const navigate = useNavigate();
+    const {user, isAuthenticated, logoutFn} = useAuth();
 
     // 이미지 관련 상태
     // imageFile : 업로드 할 이미지 파일을 따로 저장
     // imageUrl : 클라이언트가 input 창에 넣어 준 데이터
-    const [uploadedBoardImageFile, setUploadedBoardImageFile] = useState(null); // 실제 데이터베이스에 업로드하고, 파일 폴더에 저장할 이미지 파일
-    const [boardImagePreview, setBoardImagePreview] = useState(null); // 이미지 미리보기
+
+    // 메인 이미지 관련
+    const mainImgsFileInputRef = useRef(null);
+    const [uploadedMainBoardImageFiles, setUploadedMainBoardImageFiles] = useState(null); // 실제 데이터베이스에 업로드하고, 파일 폴더에 저장할 이미지 파일
+    const [boardMainImagePreviews, setBoardMainImagePreviews] = useState(null); // 이미지 미리보기
+
+    // 상세 이미지 관련
+    const detailImgsFileInputRef = useRef(null);
+    const [uploadedDetailBoardImageFiles, setUploadedDetailBoardImageFiles] = useState(null);
+    const [boardDetailImagePreviews, setBoardDetailImagePreviews] = useState([]);
 
     // js 는 컴파일 형태가 아니기 때문에, 변수 정의는 순차적으로 진행하므로, user 를 먼저 호출하고 나서
     // user 관련된 데이터 활용
-    const [boards, setBoards] = useState({
+    const [board, setBoard] = useState({
         title: '',
         content: '',
         writer: user?.memberEmail || '',
@@ -75,15 +84,6 @@ const BoardWrite = () => {
      * 특징 : JSON + 파일 데이터를 함께 전송 가능 multipart/form-data
      * 예시 : 제목, 내용(JSON) + 이미지 파일을 한 번에 전송
      */
-    
-    const [formData, setFormData] = useState({
-        title: '',
-        content: '',
-        writer: user?.memberEmail || '',
-        imageUrl:'',
-    })
-
-    const fileInputRef = useRef(null);
 
     const handleSubmit = async (e) => {
         e.preventDefault(); // 제출 일시 중지
@@ -91,7 +91,7 @@ const BoardWrite = () => {
         try {
             const boardUploadFromData = new FormData();
             // 1. 이미지 url 을 제외한 나머지 데이터 JSON 으로 변환
-            const {imageUrl, ...boardDataWithoutImage} = boards; // boardFormData
+            const {imageUrl, ...boardDataWithoutImage} = board; // boardFormData
             // 2. 게시물 작성자에 user로 로그인 했을 때 멤버 아이디 넣기
             boardDataWithoutImage.writer = user?.memberEmail;
 
@@ -105,7 +105,7 @@ const BoardWrite = () => {
              boardUploadFromData.append('board', boardDataBlob);
 
              // 4. 이미지 파일이 있으면 formData 에 추가
-            if(uploadedBoardImageFile) boardUploadFromData.append('imageFile', uploadedBoardImageFile);
+            if(uploadedMainBoardImageFiles) boardUploadFromData.append('imageFile', uploadedMainBoardImageFiles);
             
             // 5. 백엔드 호출
             await boardSave(axios, boardUploadFromData, navigate); // 추가적인 로직이 잇을 수 있으니 async await 넣어주기
@@ -116,9 +116,55 @@ const BoardWrite = () => {
     }
 
     const handleChange = (e) => {
-        handleInputChange(e, setBoards);
+        handleInputChange(e, setBoard);
     }
 
+    // 상세 이미지 여러 장 변경 핸들러
+    const handleDetailImagesChanges = (e) => {
+        const files = Array.from(e.target.files);
+
+        // 최대 5장까지만 허용
+        if(files.length > 5) {
+            alert("상세 이미지는 최대 5장까지 업로드 가능합니다.");
+            return;
+        }
+
+        // 각 파일이 5MB가 넘는지 검증
+        for(let f of files) {
+            if(f.size > 5 * 1024 * 1024) {
+                alert(`${f.name}의 크기가 5MB를 초과합니다.`);
+                return;
+            }
+
+            if(!f.type.startsWith('image/')) {
+                alert(`${f.name}은(는) 이미지 파일이 아닙니다.`);
+                return;
+            }
+
+            // for문을 통해 모든 사진에 대한 검증이 종료되면, 상세이미지 파일에 파일 명칭 저장
+            setUploadedDetailBoardImageFiles(files);
+
+            // 미리보기 생성
+            const 미리보기할상세사진들 = [];
+            let 사진개수 = 0;
+
+            files.forEach((file, 사진순서) => {
+                const reader = new FileReader();
+                reader.onloadend = () => {
+                    미리보기할상세사진들[사진순서] = reader.result;
+                    사진개수++;
+
+                    // 모든 파일 로드 완료 시
+                    if(사진개수 === files.length) {
+                        // 미리보기화면에서 보일 수 있도록 setter를 이용해서 미리보기 변수에 저장
+                        setBoardDetailImagePreviews(미리보기할상세사진들);
+                    }
+                };
+                // 파일 하나씩 하나씩 미리보기 생성
+                reader.readAsDataURL(file);
+            })
+        }
+    }
     // ok 를 할 경우 게시물 목록으로 돌려보내기
     // 기능이 하나이기 때문에 if 다음 navigate 는 {} 생략 후 작성ㅇ
     const handleCancel = () => {
@@ -150,7 +196,7 @@ const BoardWrite = () => {
                         <input type="text"
                                id="title"
                                name="title"
-                               value={boards.title}
+                               value={board.title}
                                onChange={handleChange}
                                placeholder="제목을 입력하세요."
                                maxLength={200}
@@ -158,27 +204,16 @@ const BoardWrite = () => {
                         />
                     </label>
 
-                    <label>내용 :
-                        <textarea id="content"
-                                  name="content"
-                                  value={boards.content}
-                                  onChange={handleChange}
-                                  placeholder="내용을 입력하세요."
-                                  rows={15}
-                                  required
-                        />
-                    </label>
-
                     <div className="form-group">
                         <label htmlFor="imageUrl" className="btn-upload">
-                            게시물 이미지 추가하기
+                            게시물 메인 이미지 추가하기
                         </label>
                         <input
                             type="file"
                             id="imageUrl"
                             name="imageUrl"
-                            ref={fileInputRef}
-                            onChange={handleChangeImage(setBoardImagePreview, setUploadedBoardImageFile, setBoards)}
+                            ref={mainImgsFileInputRef}
+                            onChange={handleChangeImage(setBoardMainImagePreviews, setUploadedMainBoardImageFiles, setBoard)}
                             accept="image/*"
                             style={{display: 'none'}}
                         />
@@ -186,10 +221,10 @@ const BoardWrite = () => {
                             게시물 이미지를 업로드 하세요. (최대 5MB, 이미지 파일만 가능)
                         </small>
 
-                        {boardImagePreview && (
+                        {boardMainImagePreviews && (
                             <div className="image-preview">
                                 <img
-                                    src={boardImagePreview}
+                                    src={boardMainImagePreviews}
                                     alt="미리보기"
                                     style={{
                                         maxWidth: '100%',
@@ -200,6 +235,51 @@ const BoardWrite = () => {
                                         padding: '5px'
                                     }}
                                 />
+                            </div>
+                        )}
+                    </div>
+
+                    <label>내용 :
+                        <textarea id="content"
+                                  name="content"
+                                  value={board.content}
+                                  onChange={handleChange}
+                                  placeholder="내용을 입력하세요."
+                                  rows={15}
+                                  required
+                        />
+                    </label>
+
+                    {/* 상세 이미지 여러 장 업로드 & 미리보기 */}
+                    <div className="form-group">
+                        <label htmlFor="detailImages" className="btn-upload">
+                            상세 이미지 추가하기 (최대 5장)
+                        </label>
+                        <input type="file"
+                               id="detailImages"
+                               name="detailImages"
+                               ref={detailImgsFileInputRef}
+                               onChange={handleDetailImagesChanges}
+                               accept="image/*"
+                               multiple
+                               style={{display:'none'}}
+                        />
+                        <small className="form-hint">
+                            상세 이미지를 업로드하세요. (최대 5개, 각 5MB 이하)
+                        </small>
+                        {boardDetailImagePreviews.length > 0 && (
+                            <div className="multiple-images-preview">
+                                <p className="detail-images-selected-text">
+                                    선택된 이미지 : {boardDetailImagePreviews.length} 개
+                                </p>
+
+                                {boardDetailImagePreviews.map((image, index) => (
+                                    <div key={index} className="detail-image-item">
+                                        <img src={image}
+                                             alt={`상세이미지 ${index + 1}`}
+                                        />
+                                    </div>
+                                ))}
                             </div>
                         )}
                     </div>
